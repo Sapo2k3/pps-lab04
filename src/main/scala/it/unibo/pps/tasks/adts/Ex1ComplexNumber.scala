30c91091f39a48a9c5163b07c149c062451c84c4
Mattia Saponaro <mattia.saponaro2@studio.unibo.it>
2026-03-26 10:28:35 +0100

    Implemented Complex trait

--- src/main/scala/it/unibo/pps/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/it/unibo/pps/tasks/adts/Ex1ComplexNumber.scala
@@ -19,12 +19,19 @@ object Ex1ComplexNumbers:
 
   object BasicComplexADT extends ComplexADT:
 
+    case class Compl(re: Double, im: Double)
+
     // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    type Complex = Compl
+    def complex(re: Double, im: Double): Complex = Compl(re, im)
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex.re
+      def im(): Double = complex.im
+      def sum(other: Complex): Complex = Compl(complex.re() + other.re(), complex.im() + other.im())
+      def subtract(other: Complex): Complex = Compl(complex.re() - other.re(), complex.im() - other.im())
+      def asString(): String = Compl(re, im) match
+        case Compl(0.0, 0.0) => "0.0"
+        case Compl(0.0, i) => s"${i}i"
+        case Compl(r, 0.0) => s"$r"
+        case Compl(r, i) if i > 0 => s"$r + ${i}i"
+        case Compl(r, i) if i < 0 => s"$r - ${i.abs}i"
